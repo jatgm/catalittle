627199dc9c969d073237e0e9b79e736c1679385a
Jason Zhou <jasonhaozhou@gmail.com>
2026-08-26 19:30:34 -0700

    Initial Commit

---
+++ Catalittle/CatalittleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalittleApp.swift
+//  Catalittle
+//
+//  Created by Jason Zhou on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CatalittleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Catalittle/SpinComponent.swift
@@ -0,0 +1,13 @@
+//
+//  SpinComponent.swift
+//  Catalittle
+//
+//  Created by Jason Zhou on 8/26/26.
+//
+
+import RealityKit
+
+/// A component that spins the entity around a given axis.
+struct SpinComponent: Component {
+    let spinAxis: SIMD3<Float> = [0, 1, 0]
+}
